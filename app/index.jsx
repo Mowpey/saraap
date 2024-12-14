6d347bdb905d01739c728bccb62a1436cd0e6780
Dawntless <66586346+Mowpey@users.noreply.github.com>
2024-12-14 17:59:47 +0800

    Added a splash screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,50 @@
-import { Redirect } from "expo-router";
+import { View, StyleSheet, Image } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import splashImage from "@/assets/images/splash.jpg";
 
-export default function Index() {
-  return <Redirect href={"/signin"} />;
-}
+const SplashScreen = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/signin");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <SafeAreaProvider style={styles.safeAreaProviderStyle}>
+      <SafeAreaView style={styles.safeAreaContainer}>
+        <View style={styles.splashContainer}>
+          <Image source={splashImage} style={styles.imageStyle} />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeAreaProviderStyle: {
+    backgroundColor: "white",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  safeAreaContainer: {
+    alignItems: "center",
+  },
+  splashContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  imageStyle: {
+    aspectRatio: 1,
+    width: 350,
+    height: 350,
+  },
+});
+
+export default SplashScreen;
