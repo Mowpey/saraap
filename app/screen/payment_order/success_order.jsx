c89e2384694451dc1b8b2db76aa77b96a4658245
Dawntless <66586346+Mowpey@users.noreply.github.com>
2024-12-14 20:09:34 +0800

    Update success_order.jsx

--- app/screen/payment_order/success_order.jsx
+++ app/screen/payment_order/success_order.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { scale, verticalScale, moderateScale,horizontalScale } from 'react-native-size-matters';
-import { useFonts } from 'expo-font';
+import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  scale,
+  verticalScale,
+  moderateScale,
+  horizontalScale,
+} from "react-native-size-matters";
+import { useFonts } from "expo-font";
 
 const PaymentScreen = () => {
   const [customFonts] = useFonts({
@@ -17,7 +22,7 @@ const PaymentScreen = () => {
   return (
     <View style={styles.container}>
       <Image
-        source={require('@/assets/images/deliverymotor.png')}
+        source={require("@/assets/images/deliverymotor.png")}
         style={styles.image}
       />
       <Text style={styles.title}>You've Made Order</Text>
@@ -25,11 +30,17 @@ const PaymentScreen = () => {
         Just stay at home while we are preparing your best foods
       </Text>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.button, styles.grayButton]}>
+        <TouchableOpacity
+          href="/tabs"
+          style={[styles.button, styles.grayButton]}
+        >
           <Text style={styles.buttonText}>Order Other Foods</Text>
         </TouchableOpacity>
-        
-        <TouchableOpacity href="/screen/payment_order/payment_progress" style={[styles.button, styles.purpleButton]}>
+
+        <TouchableOpacity
+          href="/screen/payment_order/payment_progress"
+          style={[styles.button, styles.purpleButton]}
+        >
           <Text style={styles.buttonTextPurp}>View My Order</Text>
         </TouchableOpacity>
       </View>
@@ -40,62 +51,61 @@ const PaymentScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f2f2f2',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
     paddingHorizontal: scale(20),
   },
   image: {
-    marginTop:verticalScale(-70),
+    marginTop: verticalScale(-70),
     transform: [{ scale: 0.7 }],
   },
   title: {
-    fontFamily: 'Poppins-Medium',
+    fontFamily: "Poppins-Medium",
     fontSize: moderateScale(18),
     marginBottom: verticalScale(-5),
-    marginTop:verticalScale(-30)
+    marginTop: verticalScale(-30),
   },
   description: {
-    fontFamily: 'Poppins-Regular',
+    fontFamily: "Poppins-Regular",
     fontSize: moderateScale(14),
-    textAlign: 'center',
+    textAlign: "center",
     marginVertical: verticalScale(10),
-    color: '#666',
-    marginHorizontal:moderateScale(15)
-
+    color: "#666",
+    marginHorizontal: moderateScale(15),
   },
   buttonContainer: {
-    flexDirection: 'column',
-    justifyContent: 'center', 
-    alignItems: 'center', 
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: verticalScale(20),
-    width: '100%',
+    width: "100%",
   },
   button: {
     paddingVertical: verticalScale(12),
     paddingHorizontal: scale(20),
     borderRadius: moderateScale(10),
-    alignItems: 'center', 
-    width: '65%', 
+    alignItems: "center",
+    width: "65%",
     marginBottom: verticalScale(10),
   },
   grayButton: {
-    backgroundColor: '#020452',
+    backgroundColor: "#020452",
   },
   purpleButton: {
-    backgroundColor: '#fff',
-    borderColor:'#6c63ff',
-    borderWidth:1,
+    backgroundColor: "#fff",
+    borderColor: "#6c63ff",
+    borderWidth: 1,
   },
   buttonText: {
-    fontFamily: 'Poppins-Medium',
+    fontFamily: "Poppins-Medium",
     fontSize: moderateScale(14),
-    color: '#fff',
+    color: "#fff",
   },
   buttonTextPurp: {
-    fontFamily: 'Poppins-Medium',
+    fontFamily: "Poppins-Medium",
     fontSize: moderateScale(14),
-    color: '#6c63ff',
+    color: "#6c63ff",
   },
 });
 
